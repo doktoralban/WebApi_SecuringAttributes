5d878ae17fb6ee4c17ab9fc2ff45c2110880dc78
Aydın Türker <doktoralban@outlook.com>
2021-01-04 15:28:23 +0300

    Add project files.

---
+++ WebApi_SecuringAttributes/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi_SecuringAttributes.Attributes;
+using WebApi_SecuringAttributes.Data;
+
+namespace WebApi_SecuringAttributes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    [ApiKey]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly dbContextNW _context;
+
+        public ProductsController(dbContextNW context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+      
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+         
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProduct(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ WebApi_SecuringAttributes/Data/dbContextNW.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_SecuringAttributes.Data
+{
+	public partial class dbContextNW : DbContext
+	{
+		public dbContextNW(DbContextOptions<dbContextNW> options) : base(options)
+		{
+
+		}
+
+
+		public DbSet<Products> Products { get; set; }
+
+
+	}
+}
